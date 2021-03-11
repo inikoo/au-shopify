4949cb44005a72cd5bd86e583388b0094b2645d9
Raul Perusquia <raul@inikoo.com>
2021-03-12 01:48:23 +0800

    storege

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,24 @@
 @section('content')
     <!-- You are: (shop domain name) -->
     <p>You are: {{ Auth::user()->name }}</p>
+
+    <form method="POST" action="/profile">
+        @csrf
+
+
+        <label for="title">Access code</label>
+
+        <input id="title" type="text" class="@error('title') is-invalid @enderror">
+        @error('title')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
+        <div class="form-group">
+            <button class="btn btn-success save-data">Save</button>
+        </div>
+    </form>
+
+
 @endsection
 
 @section('scripts')
